2b4bbd6117f5f30c42ac5be62b7d05e33d06a5e3
Sumit Maurya <smithdashdash@gmail.com>
2024-02-26 15:19:43 +0530

    Implemented done section

--- src/components/dashboard/board/TaskCard/TaskCard.jsx
+++ src/components/dashboard/board/TaskCard/TaskCard.jsx
@@ -1,41 +1,55 @@
-import React, {  useState } from 'react'
-import styles from './taskcard.module.css'
-import axios from 'axios'
-import { BASE_URL, compareDate, findColor, getFormattedDate } from '../../../../utils/constants/constant'
-import useTaskContext from '../../../../hooks/useTaskContext'
-import ARROW_DOWN_IMG from '../../../../utils/assests/arrow-down.png'
-import ARROW_UP_IMG from '../../../../utils/assests/arrow-up.png'
-import toast,{ Toaster } from 'react-hot-toast'
-const TaskCard = ({card,task,isExpanded, onToggle}) => {
-  const [showDropDown,setDropDown] = useState(false);
-  const allCategories = ['BACKLOG', 'TO-DO', 'PROGRESS', 'DONE'];
+import React, { useState } from "react";
+import styles from "./taskcard.module.css";
+import axios from "axios";
+import {
+  BASE_URL,
+  compareDate,
+  findColor,
+  getFormattedDate,
+} from "../../../../utils/constants/constant";
+import useTaskContext from "../../../../hooks/useTaskContext";
+import ARROW_DOWN_IMG from "../../../../utils/assests/arrow-down.png";
+import ARROW_UP_IMG from "../../../../utils/assests/arrow-up.png";
+import toast, { Toaster } from "react-hot-toast";
+const TaskCard = ({ card, task, isExpanded, onToggle }) => {
+  const [showDropDown, setDropDown] = useState(false);
+  const allCategories = ["BACKLOG", "TO-DO", "PROGRESS", "DONE"];
 
   const dueDateStatus = compareDate(card?.dueDate);
-  
+
   const formattedDueDate = getFormattedDate(card?.dueDate);
 
-  const {setEditTask,setTaskTitle,setSelectedPriority,setChecklists,
-  setCountCompletedTask,setDueDate,setShowDeleteTask,setTaskId} = useTaskContext();
+  const {
+    setEditTask,
+    setTaskTitle,
+    setSelectedPriority,
+    setChecklists,
+    setCountCompletedTask,
+    setDueDate,
+    setShowDeleteTask,
+    setTaskId,
+  } = useTaskContext();
   const priorityColor = findColor(card?.priority);
-  
-  const filteredCategories = allCategories.filter((cat) => cat !== card?.category);
-  const handleDelete =()=>{
+
+  const filteredCategories = allCategories.filter(
+    (cat) => cat !== card?.category
+  );
+  const handleDelete = () => {
     setShowDeleteTask(true);
     setDropDown(false);
     setTaskId(card._id);
-  }
+  };
 
-  const handleShare =()=>{
-    const textToCopy = `http://localhost:3000/dashboard/user/${card._id}`; 
+  const handleShare = () => {
+    const textToCopy = `http://localhost:3000/dashboard/user/${card._id}`;
 
-    navigator.clipboard.writeText(textToCopy)
-      .then(() => {
-        toast.success("Link copied");
-      })
-      setDropDown(false);
-  }
-  
-  const handleEdit =()=>{
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      toast.success("Link copied");
+    });
+    setDropDown(false);
+  };
+
+  const handleEdit = () => {
     setEditTask(true);
     const title = card?.title;
     const count = card?.countCompletedTask;
@@ -44,94 +58,111 @@ const TaskCard = ({card,task,isExpanded, onToggle}) => {
     setChecklists(card.checklists);
     setCountCompletedTask(count);
     setDueDate(card.dueDate);
-  }
-  const handleMoveTask = async(category,taskId)=>{
+  };
+  const handleMoveTask = async (category, taskId) => {
     try {
-      const data = {taskId,category};
-      const response = await axios.patch(`${BASE_URL}/user/category/update`,data);
+      const data = { taskId, category };
+      const response = await axios.patch(
+        `${BASE_URL}/user/category/update`,
+        data
+      );
       console.log(data);
       console.log(response);
-    } catch (error) {
-      
-    }
-  }
+    } catch (error) {}
+  };
 
-  useState(()=>{
-
-  },[task])
+  useState(() => {}, [task]);
   const title = card?.title;
   const count = card?.countCompletedTask;
   const totalChecklist = card?.checklists?.length;
   return (
     <div className={styles.card__container}>
-      <Toaster/>
+      <Toaster />
       <div className={styles.card__header}>
         <div className={styles.priority__container}>
-        <div className={styles.color__container}>
-        <div className={styles.priority__color}
-        style={{backgroundColor:priorityColor}}/>
-        {card.priority} Priority
-        </div>
-        <div className={styles.card__dropdown}
-        onClick={()=>setDropDown((prev)=>!prev)}
-        >...
-        </div>
-        </div>
-        {showDropDown&&(<div className={styles.card__dropdown__menu}>
-          <div onClick={handleEdit}
-          className={styles.edit__button}
-          >Edit</div>
-          <div className={styles.share__button}
-          onClick={handleShare}
-          >
-          Share
+          <div className={styles.color__container}>
+            <div
+              className={styles.priority__color}
+              style={{ backgroundColor: priorityColor }}
+            />
+            {card.priority} Priority
           </div>
-          <div className={styles.delete__button}
-          onClick={handleDelete}
+          <div
+            className={styles.card__dropdown}
+            onClick={() => setDropDown((prev) => !prev)}
           >
-          Delete
+            ...
+          </div>
         </div>
-        </div>)}
+        {showDropDown && (
+          <div className={styles.card__dropdown__menu}>
+            <div onClick={handleEdit} className={styles.edit__button}>
+              Edit
+            </div>
+            <div className={styles.share__button} onClick={handleShare}>
+              Share
+            </div>
+            <div className={styles.delete__button} onClick={handleDelete}>
+              Delete
+            </div>
+          </div>
+        )}
       </div>
-      <div className={styles.card__title}>
-        {title}
-      </div>
+      <div className={styles.card__title}>{title}</div>
       <div className={styles.checklist__container}>
         Checklist ({count}/{totalChecklist})
-        <div className={styles.toggle__button}
-        onClick={onToggle}
-        >
-          {isExpanded ? <img src={ARROW_UP_IMG} alt='arrow-up'/>:
-          <img src={ARROW_DOWN_IMG} alt='arrow-down'/>
-          }
+        <div className={styles.toggle__button} onClick={onToggle}>
+          {isExpanded ? (
+            <img src={ARROW_UP_IMG} alt="arrow-up" />
+          ) : (
+            <img src={ARROW_DOWN_IMG} alt="arrow-down" />
+          )}
+        </div>
       </div>
-      </div>
-      {isExpanded && card?.checklists?.map((item, index) => (
-      <div className={styles.card__checklist} key={item._id}>
-    <input 
-      type='checkbox'
-      className={styles.checklist__text}
-      checked={item?.checked}
-    />
-    <div>{item.text}</div>
-  </div>
-))}
+      {isExpanded &&
+        card?.checklists?.map((item, index) => (
+          <div className={styles.card__checklist} key={item._id}>
+            <input
+              type="checkbox"
+              className={styles.checklist__text}
+              checked={item?.checked}
+            />
+            <div>{item.text}</div>
+          </div>
+        ))}
       <div className={styles.card__footer}>
-        {card?.dueDate ? (<div className={styles.card__date}
-        style={{backgroundColor: dueDateStatus ? "#DBDBDB" : "#CF3636",
-        color: dueDateStatus ? "#5A5A5A":"#FFFFFF"
-        }}
-        >
-        {formattedDueDate}
-        </div>)
-        :(<div className={styles.empty__box}> </div>)
-        }
+        {card?.dueDate ? (
+          <>
+            {card?.category !== "DONE" ? (
+              <div
+                className={styles.card__date}
+                style={{
+                  backgroundColor: dueDateStatus ? "#DBDBDB" : "#CF3636",
+                  color: dueDateStatus ? "#5A5A5A" : "#FFFFFF",
+                }}
+              >
+                {formattedDueDate}
+              </div>
+            ) : 
+            (
+              <div
+              className={styles.card__date}
+              style={{
+                backgroundColor:"#63C05B",
+                color:"white"
+              }}
+              >{formattedDueDate}</div>
+            )
+            }
+          </>
+        ) : (
+          <div className={styles.empty__box}></div>
+        )}
         <div className={styles.card__footer__controls}>
-          {filteredCategories.map((cat)=>(
-            <div className={styles.category__button}
-            onClick={()=>handleMoveTask(
-            cat
-            ,card._id)}
+          {filteredCategories.map((cat) => (
+            <div
+              className={styles.category__button}
+              onClick={() => handleMoveTask(cat, card._id)}
             >
               {cat}
             </div>
@@ -139,7 +170,7 @@ const TaskCard = ({card,task,isExpanded, onToggle}) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TaskCard
+export default TaskCard;
--- src/components/dashboard/board/done/Done.jsx
+++ src/components/dashboard/board/done/Done.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './done.module.css'
+import useTaskContext from '../../../../hooks/useTaskContext';
+import TaskCard from '../TaskCard/TaskCard';
 const Done = () => {
+  const {setShowCreateTask,task}
+     = useTaskContext();
+    const [expandedCards, setExpandedCards] = useState([]);
+    console.log("task in todo",task);
+    useEffect(()=>{
+
+    },[task])
+
+    const handleToggle = (cardId) => {
+      setExpandedCards((prevExpanded) =>
+        prevExpanded.includes(cardId)
+          ? prevExpanded.filter((id) => id !== cardId)
+          : [...prevExpanded, cardId]
+      );
+    };
+  
+    const handleCollapseAll = () => {
+      setExpandedCards([]);
+    };
   return (
     <div className={styles.done__main}>
-    done
-    <div className={styles.testing}>
-        {[0,1].map(()=>(
-            <div>hello</div>
-        ))}
+    <div className={styles.backlog__container}>
+        <div className={styles.backlog__header}>
+        <span className={styles.backlog__heading}>
+            Done
+        </span>
+
+        <div className={styles.header__controller}>
+        <div className={styles.todo__minimize__button}
+        onClick={handleCollapseAll}
+        >
+            -
+        </div>
+        </div>
+        </div>
+    <div className={styles.card__container}>
+    {task
+      ?.filter(task => task.category === "DONE")
+      ?.map(item => (
+        <TaskCard card={item} task={task}
+        isExpanded={expandedCards.includes(item._id)}
+        onToggle={() => handleToggle(item._id)}
+        />
+      ))}
     </div>
-</div>
+    </div>
+  </div>
   )
 }
 
