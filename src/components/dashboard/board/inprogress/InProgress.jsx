ac5457e9f4a938bae5098400376fac2fa4208658
Sumit Maurya <smithdashdash@gmail.com>
2024-02-26 14:53:47 +0530

    implement in progress section

--- src/components/dashboard/board/inprogress/InProgress.jsx
+++ src/components/dashboard/board/inprogress/InProgress.jsx
@@ -1,13 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './inprogress.module.css'
+import useTaskContext from '../../../../hooks/useTaskContext';
+import TaskCard from '../TaskCard/TaskCard';
 const InProgress = () => {
+  const {setShowCreateTask,task}
+     = useTaskContext();
+    const [expandedCards, setExpandedCards] = useState([]);
+    console.log("task in todo",task);
+    useEffect(()=>{
+
+    },[task])
+
+    const handleToggle = (cardId) => {
+      setExpandedCards((prevExpanded) =>
+        prevExpanded.includes(cardId)
+          ? prevExpanded.filter((id) => id !== cardId)
+          : [...prevExpanded, cardId]
+      );
+    };
+  
+    const handleCollapseAll = () => {
+      setExpandedCards([]);
+    };
   return (
     <div className={styles.inprogress__main}>
-    inprogress
-    <div className={styles.testing}>
-        {[0,1].map(()=>(
-            <div>hello</div>
-        ))}
+    <div className={styles.backlog__container}>
+        <div className={styles.backlog__header}>
+        <span className={styles.backlog__heading}>
+            In Progress
+        </span>
+
+        <div className={styles.header__controller}>
+        <div className={styles.todo__minimize__button}
+        onClick={handleCollapseAll}
+        >
+            -
+        </div>
+        </div>
+        </div>
+    <div className={styles.card__container}>
+    {task
+      ?.filter(task => task.category === "PROGRESS")
+      ?.map(item => (
+        <TaskCard card={item} task={task}
+        isExpanded={expandedCards.includes(item._id)}
+        onToggle={() => handleToggle(item._id)}
+        />
+      ))}
+    </div>
     </div>
 </div>
   )
